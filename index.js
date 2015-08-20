b1c7a681df91d7546f109b62fd21cce4531cc079
Christian Tegnér <christian.tegner@gmail.com>
2015-08-20 17:47:35 +0200

    Conform to 'standard' codestyle 2/2: whitepsace
    I'm getting claustrophobic!

--- index.js
+++ index.js
@@ -4,38 +4,38 @@ const replacer = new RegExp(expr, 'g')
 const inline = new RegExp(`\\b(${expr})+\\b`, 'g')
 const identity = v => v
 const mapper = {
-    ms: 1,
-    s: 1000,
-    m: 60 * 1000,
-    h: 60 * 60 * 1000,
-    d: 24 * 60 * 60 * 1000,
-    w: 7 * 24 * 60 * 60 * 1000,
-    M: 30 * 24 * 60 * 60 * 1000, // Note: assumes 30 days in a month
-    y: 365 * 24 * 60 * 60 * 1000
+  ms: 1,
+  s: 1000,
+  m: 60 * 1000,
+  h: 60 * 60 * 1000,
+  d: 24 * 60 * 60 * 1000,
+  w: 7 * 24 * 60 * 60 * 1000,
+  M: 30 * 24 * 60 * 60 * 1000, // Note: assumes 30 days in a month
+  y: 365 * 24 * 60 * 60 * 1000
 }
 
-function parse(shorthand) {
-    const parts = []
-    shorthand.replace(replacer, (match, num, unit) => parts.push([parseInt(num, 10), unit]))
-    return parts
+function parse (shorthand) {
+  const parts = []
+  shorthand.replace(replacer, (match, num, unit) => parts.push([parseInt(num, 10), unit]))
+  return parts
 }
 
-function toMillis(parts) {
-    return parts.reduce((millis, [amount, unit]) => millis + (amount * mapper[unit]), 0)
+function toMillis (parts) {
+  return parts.reduce((millis, [amount, unit]) => millis + (amount * mapper[unit]), 0)
 }
 
 export default shorthand => {
-    if (matcher.test(shorthand)) {
-        return toMillis(parse(shorthand))
-    } else {
-        throw new Error(`Unrecognised shorthand: ${shorthand}`)
-    }
+  if (matcher.test(shorthand)) {
+    return toMillis(parse(shorthand))
+  } else {
+    throw new Error(`Unrecognised shorthand: ${shorthand}`)
+  }
 }
 
-export function replace(str, cb = identity) {
-    return str.replace(inline, match => {
-        const parts = parse(match)
-        return cb(toMillis(parts), parts)
-    })
+export function replace (str, cb = identity) {
+  return str.replace(inline, match => {
+    const parts = parse(match)
+    return cb(toMillis(parts), parts)
+  })
 }
 
