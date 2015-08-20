68597ee34b9da20e561382d78eb114eae8775510
Christian Tegnér <christian.tegner@gmail.com>
2015-08-20 17:45:49 +0200

    Conform to 'standard' codestyle 1/2: semicolons
    Help, where have all my semicolons gone?!

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const expr = '([0-9]+)(ms|s|m|h|d|w|M|y)';
-const matcher = new RegExp(`^(${expr})+$`);
-const replacer = new RegExp(expr, 'g');
-const inline = new RegExp(`\\b(${expr})+\\b`, 'g');
-const identity = v => v;
+const expr = '([0-9]+)(ms|s|m|h|d|w|M|y)'
+const matcher = new RegExp(`^(${expr})+$`)
+const replacer = new RegExp(expr, 'g')
+const inline = new RegExp(`\\b(${expr})+\\b`, 'g')
+const identity = v => v
 const mapper = {
     ms: 1,
     s: 1000,
@@ -12,30 +12,30 @@ const mapper = {
     w: 7 * 24 * 60 * 60 * 1000,
     M: 30 * 24 * 60 * 60 * 1000, // Note: assumes 30 days in a month
     y: 365 * 24 * 60 * 60 * 1000
-};
+}
 
 function parse(shorthand) {
-    const parts = [];
-    shorthand.replace(replacer, (match, num, unit) => parts.push([parseInt(num, 10), unit]));
-    return parts;
+    const parts = []
+    shorthand.replace(replacer, (match, num, unit) => parts.push([parseInt(num, 10), unit]))
+    return parts
 }
 
 function toMillis(parts) {
-    return parts.reduce((millis, [amount, unit]) => millis + (amount * mapper[unit]), 0);
+    return parts.reduce((millis, [amount, unit]) => millis + (amount * mapper[unit]), 0)
 }
 
 export default shorthand => {
     if (matcher.test(shorthand)) {
-        return toMillis(parse(shorthand));
+        return toMillis(parse(shorthand))
     } else {
-        throw new Error(`Unrecognised shorthand: ${shorthand}`);
+        throw new Error(`Unrecognised shorthand: ${shorthand}`)
     }
 }
 
 export function replace(str, cb = identity) {
     return str.replace(inline, match => {
-        const parts = parse(match);
-        return cb(toMillis(parts), parts);
-    });
+        const parts = parse(match)
+        return cb(toMillis(parts), parts)
+    })
 }
 
